44cfb91e6ad443710ad8be67cea49d9928d73f0c
Venkat-015 <venkat15003sridhar@gmail.com>
2024-05-04 12:00:07 +0530

    netlify

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const stripe=require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 exports.handler=async(event)=>{
     try{
       const {amount}=JSON.parse(event.body)
@@ -8,18 +9,17 @@ exports.handler=async(event)=>{
         currency:'usd',
         payment_method_types:['card'],
       });
-     return(
-        
-        {statusCode:200,
+     return{
+            statusCode:200,
         body:JSON.stringify({paymentIntent}),
-    });
+    };
     }
     catch(error){
         console.log({error});
-        return({
-        status:400,
+        return{
+        statusCode:400,
         body:JSON.stringify({error}),
-        });
+        };
 
     }
 }
--- src/component/payment-form/payment-form.component.jsx
+++ src/component/payment-form/payment-form.component.jsx
@@ -9,10 +9,37 @@ const PaymentForm=()=>{
     if(!stripe || !elements){
         return;
     }
+    const response=await fetch('/.netlify/functions/create-payment-intent',{
+      method:'post',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({amount:10000})
+    }).then((res)=>res.json());
+    console.log(response);
+    const {paymentIntent:{client_secret}}=response;
+    console.log(client_secret);
+    const paymentResult=await stripe.confirmCardPayment(client_secret,{
+        payment_method:{
+            card:elements.getElement(CardElement),
+            billing_details:{
+                name:'Venkat',
+            },
+        },
+    });
+    if(paymentResult.error){
+        alert(paymentResult.error);
     }
+    else{
+        if(paymentResult.paymentIntent.status==='succeeded'){
+            alert('Payment Successful');
+        }
+    }
+    };
+    
 return(
     <PaymentFormContainer>
-        <FormContainer>
+        <FormContainer onSubmit={paymentHandler}>
             <h2>Credit Card Payment:</h2>
         <CardElement /><br/>
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now!</Button>
