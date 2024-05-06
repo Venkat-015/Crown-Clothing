7c552bb54b4c31763576ecc59cb74cebd625f76b
Venkat-015 <venkat15003sridhar@gmail.com>
2024-05-06 15:16:47 +0530

    stripe

--- src/component/button/button.styles.jsx
+++ src/component/button/button.styles.jsx
@@ -1,30 +1,32 @@
-import styled from 'styled-components';
-//import { SpinnerContainer } from '../spinner/spinner.styles';
-export const BaseButton=styled.button`
-min-width: 165px;
-    width: auto;
-    height: 50px;
-    letter-spacing: 0.5px;
-    line-height: 50px;
-    padding: 0 35px 0 35px;
-    font-size: 15px;
-    background-color: black;
-    color: white;
-    text-transform: uppercase;
-    font-family: 'Open Sans Condensed';
-    font-weight: bolder;
-    border: none;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items:center;
-  
-    &:hover {
-      background-color: white;
-      color: black;
-      border: 1px solid black;}
+import styled from "styled-components";
+import {SpinnerContainer} from "../spinner/spinner.styles"
+export const BaseButton = styled.button`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  background-color: black;
+  color: white;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
 `;
-export const GoogleSignInButton=styled(BaseButton)`
+
+export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
   color: white;
 
@@ -33,7 +35,8 @@ export const GoogleSignInButton=styled(BaseButton)`
     border: none;
   }
 `;
-export const InvertedButton=styled(BaseButton)`
+
+export const InvertedButton = styled(BaseButton)`
   background-color: white;
   color: black;
   border: 1px solid black;
@@ -44,22 +47,9 @@ export const InvertedButton=styled(BaseButton)`
     border: none;
   }
 `;
-export const ButtonSpinner=styled.div`
-height:30px;
-width:30px;
-display: inline-block;
-  border: 3px solid rgba(195, 195, 195, 0.6);
-  border-radius: 50%;
-  border-top-color: #636767;
-  animation: spin 1s ease-in-out infinite;
-  -webkit-animation: spin 1s ease-in-out infinite;
-  @keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }`;
+
+export const LoadingSpinner =styled(SpinnerContainer)`
+  width: 30px;
+  height: 30px;
+  
+`;
--- src/component/payment-form/payment-form.styles.jsx
+++ src/component/payment-form/payment-form.styles.jsx
@@ -1,17 +1,21 @@
-import styled from "styled-components";
-import Button from "../button/button.component";
-export const PaymentFormContainer=styled.div`
-height:300px;
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
+import styled from 'styled-components';
+
+import Button from '../button/button.component';
+
+export const PaymentFormContainer = styled.div`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
-export const FormContainer=styled.div`
-height:100px;
-min-width:500px;
+
+export const FormContainer = styled.form`
+  height: 100px;
+  min-width: 500px;
 `;
-export const PaymentButton=styled(Button)`
-margin-top:30px;
-margin-left:auto;
-`
+
+export const PaymentButton = styled(Button)`
+  margin-left: auto;
+  margin-top: 30px;
+`;
